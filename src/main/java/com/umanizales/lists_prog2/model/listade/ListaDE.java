ba65da653e4c4b41907c7764bc4270c34a64ef55
Carlos Loaiza <carloaiza@umanizales.edu.co>
2021-10-30 11:48:32 -0500

    Se agrega el método add en Lista DE

--- src/main/java/com/umanizales/lists_prog2/model/listade/ListaDE.java
+++ src/main/java/com/umanizales/lists_prog2/model/listade/ListaDE.java
@@ -2,6 +2,7 @@ package com.umanizales.lists_prog2.model.listade;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.umanizales.lists_prog2.model.Boy;
 
 @Data
 @NoArgsConstructor
@@ -9,5 +10,23 @@ public class ListaDE {
     private Node head;
     private int count;
 
-
+    public void add(Boy boy)
+    {
+        if(this.head!=null)
+        {
+            Node temp = this.head;
+            while(temp.getNext()!=null)
+            {
+                temp = temp.getNext();
+            }
+            Node newBoy= new Node(boy);
+            temp.setNext(newBoy);
+            newBoy.setPrevious(temp);
+        }
+        else
+        {
+            //NO hay datos
+            this.head= new Node(boy);
+        }
+    }
 }
---
+++ src/main/java/com/umanizales/lists_prog2/service/ListaDeService.java
@@ -0,0 +1,7 @@
+package com.umanizales.lists_prog2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ListaDeService {
+}
